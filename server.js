0914692cc1755ca8d8100f4b244c8db822c4b0b8
Vivekanand <vivek8649@gmail.com>
2020-06-07 23:45:45 +0530

    added server file

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(`./dist/quotes-generator`));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/quotes-generator/' }
+  );
+});
+app.listen(process.env.PORT || 8080);
