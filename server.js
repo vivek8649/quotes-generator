04cfdb7c6b9e3633a90ad411adfc8a599c93673e
Vivekanand <vivek8649@gmail.com>
2020-06-08 10:44:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
 
-app.use(express.static(`./dist/quotes-generator`));
+app.use(express.static(__dirname + '/dist'));
+
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/quotes-generator/' }
-  );
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
